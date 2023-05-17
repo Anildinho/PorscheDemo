37f9d48064e8802a1071e045832965c5e9705349
M.A. Yalcinkaya <Anildinho@users.noreply.github.com>
2023-05-17 14:25:05 +0200

    v1.0.0.

---
+++ index.php
@@ -0,0 +1,481 @@
+<?php
+
+?>
+
+<html>
+<head>
+    <title> Overzicht modellen | Porsche Nederland </title>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<header>
+    <?php
+
+    include'functions/head.php';
+    head();
+
+    ?>
+</header>
+<body>
+
+<h2 style="padding: 30px 0 0 20px;">Model overview</h2>
+
+<div class="emptySpace"></div>
+<h1>718 Cayman Modellen</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/718_Cayman/718_cayman.png">
+            <h5>718 Cayman</h5>
+            <h5>Vanaf € 82.800,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/718_Cayman/718_cayman_s.png">
+            <h5>718 Cayman S</h5>
+            <h5>Vanaf € 101.800,00 (consumentenadviesprijs)</h5>
+        </div>
+    </div>
+</div>
+
+<div class="emptySpace"></div>
+<h1>718 Boxster Modellen</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/718_Boxster/718_boxster.png">
+            <h5>718 Boxster</h5>
+            <h5>Vanaf € 84.800,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/718_Boxster/718_boxster_s.png">
+            <h5>718 Boxster S</h5>
+            <h5>Vanaf € 104.200,00 (consumentenadviesprijs)</h5>
+        </div>
+    </div>
+</div>
+
+<div class="emptySpace"></div>
+<h1>718 T Modellen</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/718_T/718_cayman_t.png">
+            <h5>718 Caymen T</h5>
+            <h5>Vanaf € 92.600,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/718_T/718_boxster_t.png">
+            <h5>718 Boxster T</h5>
+            <h5>Vanaf € 94.600,00 (consumentenadviesprijs)</h5>
+        </div>
+    </div>
+</div>
+
+<div class="emptySpace"></div>
+<h1>718 GTS 4.0 Modellen</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/718_GTS_4_0/718_cayman_gts_4_0.png">
+            <h5>718 Cayman GTS 4.0</h5>
+            <h5>Vanaf € 125.200,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/718_GTS_4_0/718_boxster_gts_4_0.png">
+            <h5>718 Boxster GTA 4.0</h5>
+            <h5>Vanaf € 127.200,00 (consumentenadviesprijs)</h5>
+        </div>
+    </div>
+</div>
+
+<div class="emptySpace"></div>
+<h1>718 GTS 4.0 Modellen</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/718_Cayman_GT4/718_cayman_gt4.png">
+            <h5>718 Cayman GT4</h5>
+            <h5>Vanaf € 141.700,00 (consumentenadviesprijs)</h5>
+        </div>
+    </div>
+</div>
+
+<div class="emptySpace"></div>
+<h1>718 Spyder</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/718_Spyder/718_spyder.png">
+            <h5>718 Spyder</h5>
+            <h5>Vanaf € 138.800,00 (consumentenadviesprijs)</h5>
+        </div>
+    </div>
+</div>
+
+<div class="emptySpace"></div>
+<h1>911 Carrera & Targa Modellen</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/911_Carrera/911_carrera.png">
+            <h5>911 Carerra</h5>
+            <h5>Vanaf € 143.200,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/911_Carrera/911_carrera_cabriolet.png">
+            <h5>911 Carerra Cabriolet</h5>
+            <h5>Vanaf € 159.000,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/911_Carrera/911_carrera_4.png">
+            <h5>911 Carerra 4</h5>
+            <h5>Vanaf € 151.600,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/911_Carrera/911_carrera_4_cabriolet.png">
+            <h5>911 Carerra 4 Cabriolet</h5>
+            <h5>Vanaf € 167.800,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/911_Carrera/911_carrera_s.png">
+            <h5>911 Carerra S</h5>
+            <h5>Vanaf € 161.000,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/911_Carrera/911_carrera_4.png">
+            <h5>911 Carerra S Cabriolet</h5>
+            <h5>Vanaf € 151.600,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/911_Carrera/911_carrera_4s.png">
+            <h5>911 Carerra 4S</h5>
+            <h5>Vanaf € 169.400,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/911_Carrera/911_carrera_4s_cabriolet.png">
+            <h5>911 Carerra 4S Cabriolet</h5>
+            <h5>Vanaf € 185.100,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/911_Targa/911_targa_4.png">
+            <h5>911 Targa 4</h5>
+            <h5>Vanaf € 168.200,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/911_Targa/911_targa_4s.png">
+            <h5>911 Targa 4S</h5>
+            <h5>Vanaf € 186.000,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/911_Targa/911_targa_4s_heritage_design_edition.png">
+            <h5>911 Targa 4S Heritage Design Edition</h5>
+            <h5>Vanaf € 225.900,00 (consumentenadviesprijs)</h5>
+        </div>
+    </div>
+</div>
+
+<div class="emptySpace"></div>
+<h1>911 Turbo Modellen</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/911_Turbo/911_turbo.png">
+            <h5>911 Turbo</h5>
+            <h5>Vanaf € 239.200,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/911_Turbo/911_turbo_cabriolet.png">
+            <h5>911 Turbo Cabriolet</h5>
+            <h5>Vanaf € 254.600,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/911_Turbo/911_turbo_s.png">
+            <h5>911 Turbo S</h5>
+            <h5>Vanaf € 272.500,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/911_Turbo/911_turbo_s_cabriolet.png">
+            <h5>911 Turbo S Cabriolet</h5>
+            <h5>Vanaf € 287.900,00 (consumentenadviesprijs)</h5>
+        </div>
+    </div>
+</div>
+
+<div class="emptySpace"></div>
+<h1>Taycan Modellen</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Taycan/taycan_4s.png">
+            <h5>Taycan 4S</h5>
+            <h5>Vanaf € 109.900,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Taycan/taycan_turbo.png">
+            <h5>Taycan Turbo</h5>
+            <h5>Vanaf € 157.100,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Taycan/taycan_turbo_s.png">
+            <h5>Taycan Turbo S</h5>
+            <h5>Vanaf € 191.000,00 (consumentenadviesprijs)</h5>
+        </div>
+    </div>
+</div>
+
+<div class="emptySpace"></div>
+<h1>Panamera Modellen</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera/panamera_4.png">
+            <h5>Panamera 4</h5>
+            <h5>Vanaf € 136.600,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera/panamera_4_10_years_edition.png">
+            <h5>Panamera 4 10 Years Edition</h5>
+            <h5>Vanaf € 157.300,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera/panamera_4_sport_turismo.png">
+            <h5>Panamera 4 Sport Turismo</h5>
+            <h5>Vanaf € 143.200,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera/panamera_4_sport_turismo_10_years_edition.png">
+            <h5>Panamera 4 Sport Turismo 10 Years Edition</h5>
+            <h5>Vanaf € 162.600,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera/panamera_4_executive.png">
+            <h5>Panamera 4 Executive</h5>
+            <h5>Vanaf € 148.800,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera/panamera_4s.png">
+            <h5>Panamera 4S</h5>
+            <h5>Vanaf € 161.100,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera/panamera_4s_sport_turismo.png">
+            <h5>Panamera 4S Sport Turismo</h5>
+            <h5>Vanaf € 169.600,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera/panamera_4s_executive.png">
+            <h5>Panamera 4S Executive</h5>
+            <h5>Vanaf € 177.600,000 (consumentenadviesprijs)</h5>
+        </div>
+    </div>
+</div>
+
+<div class="emptySpace"></div>
+<h1>Panamera GTS Modellen</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera_GTS/panamera_gts.png">
+            <h5>Panamera GTS</h5>
+            <h5>Vanaf € 194.300,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera_GTS/panamera_gts_sport_turismo.png">
+            <h5>Panamera GTS Sport Turismo</h5>
+            <h5>Vanaf € 198.800,00 (consumentenadviesprijs)</h5>
+        </div>
+    </div>
+</div>
+
+<div class="emptySpace"></div>
+<h1>Panamera Turbo Modellen</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera_Turbo/panamera_turbo.png">
+            <h5>Panamera Turbo</h5>
+            <h5>Vanaf € 216.100,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera_Turbo/panamera_turbo_sport_turismo.png">
+            <h5>Panamera Turbo Sport Turismo</h5>
+            <h5>Vanaf € 220.300,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera_Turbo/panamera_turbo_executive.png">
+            <h5>Panamera Turbo Executive</h5>
+            <h5>Vanaf € 231.500,00 (consumentenadviesprijs)</h5>
+        </div>
+    </div>
+</div>
+
+<div class="emptySpace"></div>
+<h1>Panamera E-Hybrid Modellen</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera_E_Hybrid/panamera_4_e_hybrid.png">
+            <h5>Panamera 4 E-Hybrid</h5>
+            <h5>Vanaf € 123.000,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera_E_Hybrid/panamera_4_e_hybrid_10_years_edition.png">
+            <h5>Panamera 4 E-Hybrid 10 Years Edition</h5>
+            <h5>Vanaf € 137.900,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera_E_Hybrid/panamera_4_e_hybrid_sport_turismo.png">
+            <h5>Panamera 4 E-Hybrid Sport Turismo</h5>
+            <h5>Vanaf € 127.100,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera_E_Hybrid/panamera_4_e_hybrid_sport_turismo_10_years_edition.png">
+            <h5>Panamera 4 E-Hybrid Sport turismo 10 Years Edition</h5>
+            <h5>Vanaf € 141.400,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera_E_Hybrid/panamera_4_e_hybrid_executive.png">
+            <h5>Panamera 4 E-Hybrid Executive</h5>
+            <h5>Vanaf € 131.000,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera_E_Hybrid/panamera_turbo_s_e_hybrid.png">
+            <h5>Panamera Turbo S E-Hybrid</h5>
+            <h5>Vanaf € 202.600,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera_E_Hybrid/panamera_turbo_s_e_hybrid_sport_turismo.png">
+            <h5>Panamera Turbo S E-Hybrid Sport Turismo</h5>
+            <h5>Vanaf € 206.500,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Panamera_E_Hybrid/panamera_turbo_s_e_hybrid_executive.png">
+            <h5>Panamera Turbo S E-Hybrid Executive</h5>
+            <h5>Vanaf € 216.900,00 (consumentenadviesprijs)</h5>
+        </div>
+    </div>
+</div>
+
+<div class="emptySpace"></div>
+<h1>Macan Modellen</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Macan/macan.png">
+            <h5>Macan</h5>
+            <h5>Vanaf € 93.600 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Macan/macan_s.png">
+            <h5>Macan S</h5>
+            <h5>Vanaf € 109.100,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Macan/macan_gts.png">
+            <h5>Macan GTS</h5>
+            <h5>Vanaf € 124.700 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Macan/macan_turbo.png">
+            <h5>Macan Turbo</h5>
+            <h5>Vanaf € 138.500,00 (consumentenadviesprijs)</h5>
+        </div>
+    </div>
+</div>
+
+<div class="emptySpace"></div>
+<h1>Cayenne Modellen</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Cayenne/cayenne.png">
+            <h5>Cayenne</h5>
+            <h5>Vanaf € 125.800,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Cayenne/cayenne_e_hybird.png">
+            <h5>Cayenne E-Hybrid</h5>
+            <h5>Vanaf € 103.900,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Cayenne/cayenne_s.png">
+            <h5>Cayenne S</h5>
+            <h5>Vanaf € 144.800,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Cayenne/cayenne_gts.png">
+            <h5>Cayenne GTS</h5>
+            <h5>Vanaf € 178.900,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Cayenne/cayenne_turbo.png">
+            <h5>Cayenne Turbo</h5>
+            <h5>Vanaf € 210.200,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Cayenne/cayenne_turbo_s_e_hybird.png">
+            <h5>Cayenne Turbo S E-Hybrid</h5>
+            <h5>Vanaf € 190.200,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Cayenne/cayenne_coupe.png">
+            <h5>Cayenne Coupé</h5>
+            <h5>Vanaf € 134.200,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Cayenne/cayenne_e_hybird_coupe.png">
+            <h5>Cayenne E-Hybrid Coupé</h5>
+            <h5>Vanaf € 108.800,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Cayenne/cayenne_s_coupe.png">
+            <h5>Cayenne S Coupé</h5>
+            <h5>Vanaf € 151.600,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Cayenne/cayenne_gts_coupe.png">
+            <h5>Cayenne GTS Coupé</h5>
+            <h5>Vanaf € 183.800,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Cayenne/cayenne_turbo_coupe.png">
+            <h5>Cayenne Turbo Coupé</h5>
+            <h5>Vanaf € 215.300,00 (consumentenadviesprijs)</h5>
+        </div>
+        <div class="col-4 rowBox">
+            <img class="rowCarImg" src="img/Porsche/Cayenne/cayenne_turbo_s_e_hybird_coupe.png">
+            <h5>Cayenne Turbo S E-Hybrid Coupé</h5>
+            <h5>Vanaf € 194.000,00 (consumentenadviesprijs)</h5>
+        </div>
+    </div>
+</div>
+
+<div style="padding-bottom: 20px;"></div>
+
+</body>
+<footer>
+
+</footer>
+<script type="text/javascript">
+
+</script>
+</html>
